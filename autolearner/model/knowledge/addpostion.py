8aa2542a480c8dbdea0cd91c14358d5250b7c09d
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-11-24 13:55:05 +0800

    autolearner

--- autolearner/model/knowledge/addpostion.py
+++ autolearner/model/knowledge/addpostion.py
@@ -1,2 +1,10 @@
 import torch
 import torch.nn as nn
+
+class Adpositions(nn.Module):
+    def __init__(self, name, arity):
+        super().__init__()
+        self.arity = arity
+    
+    def forward(self, x):
+        return x
